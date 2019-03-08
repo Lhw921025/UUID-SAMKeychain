4a6e75db2dc6ded4ebf03ed2ccd17db70aa5ff90
liuhongwei <liuhongwei@jldaren.com>
2019-03-08 08:25:50 +0800

    Initial Commit

---
+++ UUID-SAMKeychain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UUID-SAMKeychain
+//
+//  Created by 刘红伟 on 2019/3/8.
+//  Copyright © 2019 刘红伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UUID-SAMKeychain/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UUID-SAMKeychain
+//
+//  Created by 刘红伟 on 2019/3/8.
+//  Copyright © 2019 刘红伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
